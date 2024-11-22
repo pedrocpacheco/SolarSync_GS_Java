6ea0d60d9bd65965cd08a09dad3d8a698c9e1acf
pedrocpacheco <cpedropacheco@hotmail.com>
2024-11-22 18:16:00 -0300

    setup ia

--- src/main/java/br/com/solarsync/javabackend/ai/SpringAIChatService.java
+++ src/main/java/br/com/solarsync/javabackend/ai/SpringAIChatService.java
@@ -4,7 +4,11 @@ import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import br.com.solarsync.javabackend.model.Cliente;
+import br.com.solarsync.javabackend.model.Empresa;
 import br.com.solarsync.javabackend.model.Servico;
+import br.com.solarsync.javabackend.repository.ClienteRepository;
+import br.com.solarsync.javabackend.repository.EmpresaRepository;
 import br.com.solarsync.javabackend.repository.ServicoRepository;
 
 @Service
@@ -14,9 +18,17 @@ public class SpringAIChatService {
   @Autowired
   private final ServicoRepository servicoRepository;
 
-  public SpringAIChatService(ChatClient.Builder chatClientBuilder, ServicoRepository servicoRepository) {
+  @Autowired
+  private final ClienteRepository clienteRepository;
+
+  @Autowired
+  private final EmpresaRepository empresaRepository;
+
+  public SpringAIChatService(ChatClient.Builder chatClientBuilder, ServicoRepository servicoRepository, ClienteRepository clienteRepository, EmpresaRepository empresaRepository) {
     this.chatClientBuilder = chatClientBuilder;
     this.servicoRepository = servicoRepository;
+    this.clienteRepository = clienteRepository;
+    this.empresaRepository = empresaRepository;
   }
 
   public String run(String userPrompt) {
@@ -30,9 +42,12 @@ public class SpringAIChatService {
   }
 
   public String getServicoDetailsById(Long id) {
-    Servico servico = servicoRepository.findById(id)
-        .orElseThrow(() -> new RuntimeException("Servico não encontrado"));
+    Servico servico = servicoRepository.findById(id).get();
+    Cliente cliente = clienteRepository.findById(servico.getIdCliente()).get();
+    Empresa empresa = empresaRepository.findById(servico.getIdEmpresa()).get();
 
-    return String.format("Quanto é 1 + 1?");
+    return String.format("Meu projeto SolarSync serve para unir tanto Clientes que desejam contratar empresas que forncem soluções energéticas, quanto empresas que estão buscando clientes para contratarem os seus serviços. Os Clientes deixam claro suas pretenções, o que eles utilizam, quem eles são, qual seu consumo energetico por dia, e as empresas fazem o mesmo. A unificação entre Cliente e Empresa é feita com o Servico. O que eu peço para você é, analisar as entidades Cliente, Empresa e Serviço a seguir, e diga-me o que pensa dessa união, se ela é eficaz. Se o consumo energetico do cliente for maior do que a produção energetica da empresa, ou se as descrições forem contraditorias (exemplo, cliente só pode alugar placas, e a empresa só pode vender), e se as localizações deles for diferente, já de um sinal negativo para essa união. | %s | %s | %s ", cliente.toString(), empresa.toString(), servico.toString());
+    
   }
 }
+
